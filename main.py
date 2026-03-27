db4e91f44041464924dc6c50edc2069061a28b3d
abdullayevv7 <abdullayev.axv@gmail.com>
2026-03-27 16:40:27 +0500

    Kod yozildi.

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
+# class User:
+#     def __init__(self, age):
+#         self._age = age
+
+#     def get_age(self):
+#         return self._age
+    
+#     def set_age(self, value):
+#         if value < 0:
+#             raise ValueError("Age manfiy bo'lishi mumkin emas")
+#         self._age = value
+
+
+# user = User(20)
+
+# print(user.get_age())
+# user.set_age(25)
+# print(user.get_age)
+
+
+
+
 class User:
     def __init__(self, age):
         self._age = age
 
-    def get_age(self):
+    @property
+    def age(self):
         return self._age
-    
-    def set_age(self, value):
+
+    @age.setter
+    def age(self, value):
         if value < 0:
             raise ValueError("Age manfiy bo'lishi mumkin emas")
         self._age = value
 
 
+
 user = User(20)
 
-print(user.get_age())
-user.set_age(25)
-print(user.get_age)
+print(user.age)  # 20
+
+user.age = 25
+print(user.age)  # 25
+
+try:
+    user.age = -5
+except ValueError as e:
+    print("Xato:", e)
